a880e072b5f29687781127ec51cfd27447292376
lolo <lpizzutobeltran@gmail.com>
2026-01-13 21:06:12 -0300

    last commit before refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,210 @@
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 
 #include <iostream>
-#include <vector>
-#include <string.h>
+#include <stdexcept>
+#include <cstdlib>
 
-const uint32_t WIN_HEIGHT = 720;
-const uint32_t WIN_WIDTH = 1280;
-const char* WIN_NAME = "engine-window";
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const char* WINDOW_NAME = "Window name";
 
-void initWindow(GLFWwindow* window);
-void cleanup(VkInstance instance, GLFWwindow* window);
+class HelloTriangleApplication {
+public:
+    void run() {
+        printf("Running...\n");
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window = nullptr;
+
+    vk::raii::Context  context;
+    vk::raii::Instance instance = nullptr;
+    vk::raii::PhysicalDevice physicalDevice = nullptr;
+    vk::raii::Device device = nullptr; float queuePriority = 0.5f;
+    vk::PhysicalDeviceFeatures deviceFeatures;
+
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    }
+
+    void initVulkan() {
+        createInstance();
+        pickPhysicalDevice();
+        createLogicalDevice();
+    }
+
+    void createInstance() {
+        vk::ApplicationInfo appInfo = {};
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = vk::ApiVersion14;
+
+        // Get the required instance extensions from GLFW.
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        // Check if the required GLFW extensions are supported by the Vulkan implementation.
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+        {
+            if (std::ranges::none_of(extensionProperties,
+                [glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
+                { return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+            {
+                throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+            }
+        }
+
+        vk::InstanceCreateInfo createInfo = {};
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+
+        instance = vk::raii::Instance(context, createInfo);
+
+        auto extensions = context.enumerateInstanceExtensionProperties();
+        std::cout << "available extensions:\n";
+
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+
+    }
+
+    void pickPhysicalDevice() {
+
+        std::vector<const char*> deviceExtensions = {
+            vk::KHRSwapchainExtensionName,
+            vk::KHRSpirv14ExtensionName,
+            vk::KHRSynchronization2ExtensionName,
+            vk::KHRCreateRenderpass2ExtensionName
+        };
+
+        auto devices = instance.enumeratePhysicalDevices();
+        if (devices.empty()) {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+
+        const auto devIter = std::ranges::find_if(devices,
+            [&](auto const& device) {
+                auto queueFamilies = device.getQueueFamilyProperties();
+                bool isSuitable = device.getProperties().apiVersion >= VK_API_VERSION_1_3;
+                const auto qfpIter = std::ranges::find_if(queueFamilies,
+                    [](vk::QueueFamilyProperties const& qfp)
+                    {
+                        return (qfp.queueFlags & vk::QueueFlagBits::eGraphics) != static_cast<vk::QueueFlags>(0);
+                    });
+                isSuitable = isSuitable && (qfpIter != queueFamilies.end());
+                auto extensions = device.enumerateDeviceExtensionProperties();
+                bool found = true;
+                for (auto const& extension : deviceExtensions) {
+                    auto extensionIter = std::ranges::find_if(extensions, [extension](auto const& ext) {return strcmp(ext.extensionName, extension) == 0; });
+                    found = found && extensionIter != extensions.end();
+                }
+                isSuitable = isSuitable && found;
+                if (isSuitable) {
+                    physicalDevice = device;
+                }
+                return isSuitable;
+            });
+
+        if (devIter == devices.end()) {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+
+        /// this is mine :>
+        for (const auto& device : devices) {
+            physicalDevice = device;
+            auto deviceProperties = device.getProperties();
+            printf(deviceProperties.deviceName);  // this shows the available gpus i think
+            if (isDeviceSuitable(device)) printf(" | Suitable device\n");
+            else printf(" | Unsuitable device\n");
+            break;
+        }
+
+
+    }
+
+    uint32_t findQueueFamilies(vk::raii::PhysicalDevice physicalDevice) {
+        // find the index of the first queue family that supports graphics
+        std::vector<vk::QueueFamilyProperties> queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
+
+        // get the first index into queueFamilyProperties which supports graphics
+        auto graphicsQueueFamilyProperty =
+            std::find_if(queueFamilyProperties.begin(),
+                queueFamilyProperties.end(),
+                [](vk::QueueFamilyProperties const& qfp) { return qfp.queueFlags & vk::QueueFlagBits::eGraphics; });
+
+        return static_cast<uint32_t>(std::distance(queueFamilyProperties.begin(), graphicsQueueFamilyProperty));
+    }
+
+    bool isDeviceSuitable(vk::raii::PhysicalDevice physicalDevice) {
+        auto deviceProperties = physicalDevice.getProperties();
+        auto deviceFeatures = physicalDevice.getFeatures();
+    
+        /// Example for checking devices
+        //if (deviceProperties.deviceType == vk::PhysicalDeviceType::eDiscreteGpu && deviceFeatures.geometryShader) {
+        //    return true;
+        //}
+        //
+        
+        if (physicalDevice != nullptr) return true;
+        return false;
+    }
+
+
+    void createLogicalDevice() {
+        std::vector<vk::QueueFamilyProperties> queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
+        uint32_t graphicsIndex = findQueueFamilies(physicalDevice);
+        
+        vk::DeviceQueueCreateInfo deviceQueueCreateInfo = {};
+            deviceQueueCreateInfo.queueFamilyIndex = graphicsIndex;
+            deviceQueueCreateInfo.queueFamilyIndex = graphicsIndex; 
+            deviceQueueCreateInfo.queueCount = 1;
+            deviceQueueCreateInfo.pQueuePriorities = &queuePriority;
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+};
 
 int main() {
+    HelloTriangleApplication app;
 
-// Window setup
-   GLFWwindow* win;
-   initWindow(win);
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-
-
-  uint32_t extCount = 0;
-  const char** ext = glfwGetRequiredInstanceExtensions(&extCount);
-  std::vector<const char*> exts(ext, ext + extCount);
-
-  VkApplicationInfo app = {
-      .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
-      .pApplicationName = "engine",
-      .applicationVersion = VK_MAKE_VERSION(0,1,0),
-      .pEngineName = "engine",
-      .engineVersion = VK_MAKE_VERSION(0,1,0),
-      .apiVersion = VK_API_VERSION_1_2,
-  };
-
-  VkInstanceCreateInfo ci{ VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
-  ci.pApplicationInfo = &app;
-  ci.enabledExtensionCount = (uint32_t)exts.size();
-  ci.ppEnabledExtensionNames = exts.data();
-
-  VkInstance instance = VK_NULL_HANDLE;
-  if (vkCreateInstance(&ci, nullptr, &instance) != VK_SUCCESS) {
-    std::cerr << "vkCreateInstance failed\n";
-    glfwDestroyWindow(win);
-    glfwTerminate();
-    return 1;
-  }
-
-  std::cout << "Vulkan instance OK\n";
-
-// Main loop
-  while (!glfwWindowShouldClose(win)) { 
-      glfwPollEvents(); 
-  }
-
-// Cleanup / Exit
-  cleanup(instance, win);
-
-  return 0;
-}
-
-
-void initWindow(GLFWwindow* window) {
-    if (!glfwInit()) { std::cerr << "GLFW init failed\n"; return 1; }
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_NAME, nullptr, nullptr);
-    if (!window) { std::cerr << "Window create failed\n"; glfwTerminate(); return 1; }
-}
-
-void initVulkan() {
-    creayeInstance();
-}
-
-void createInstance() {
-    VkApplicationInfo appInfo = { 
-        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-        .pApplicationName = "Hello Triangle",
-        .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
-        .pEngineName = "No Engine",
-        .engineVersion = VK_MAKE_VERSION(1, 0, 0),
-        .apiVersion = vkApiVersion14 
-    };
-}
-
-void cleanup(VkInstance instance, GLFWwindow* window) {
-    vkDestroyInstance(instance, nullptr);
-    glfwDestroyWindow(window);
-    glfwTerminate();
-}
-
-void initVulkan() {
-    
+    return EXIT_SUCCESS;
 }
