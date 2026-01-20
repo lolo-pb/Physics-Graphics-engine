e4f5062095ee2872ebd2b5e5677fc671dce9a8d2
lolo <lpizzutobeltran@gmail.com>
2026-01-19 22:57:09 -0300

    last boring part :D... i hope

--- src/main.cpp
+++ src/main.cpp
@@ -46,15 +46,22 @@ private:
 
     GLFWwindow* window = nullptr;
     VkInstance instance = nullptr;
+    
     VkSurfaceKHR surface = nullptr;
+    
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
     VkDevice device = nullptr;
+    
     VkQueue graphicsQueue = nullptr;
     VkQueue presentQueue = nullptr;
+    
     VkSwapchainKHR swapChain = nullptr;
     std::vector<VkImage> swapChainImages;
     VkFormat swapChainImageFormat{};
     VkExtent2D swapChainExtent{};
+
+    std::vector<VkImageView> swapChainImageViews;
+
 
 /// Init window ///
     void initWindow() {
@@ -78,6 +85,7 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
     
         printf("Vulkan initiated.\n");
     }
@@ -387,6 +395,33 @@ private:
         }
     }
 
+    void createImageViews() {
+        swapChainImageViews.resize(swapChainImages.size());
+
+        for (size_t i = 0; i < swapChainImages.size(); i++) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create image views!");
+            }
+        }
+
+    }
 
 
 /// Main Loop ///
@@ -402,6 +437,8 @@ private:
     void cleanup() {
         printf("Closing...\n");
 
+        for (auto imageView : swapChainImageViews) 
+            {vkDestroyImageView(device, imageView, nullptr);}
         vkDestroySwapchainKHR(device, swapChain, nullptr);
         vkDestroyDevice(device, nullptr);
         vkDestroySurfaceKHR(instance, surface, nullptr);
