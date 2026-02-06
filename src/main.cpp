1ce2099008c80ed745a039530870cc44426beefb
lolo <lpizzutobeltran@gmail.com>
2026-02-06 00:00:44 -0300

    vertex buffer more

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@ struct Vertex {
 
 const std::vector<Vertex> vertices = { // temporary vertices
     {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-    {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+    {{0.5f,  0.5f}, {0.0f, 1.0f, 0.0f}},
     {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
 };
 
